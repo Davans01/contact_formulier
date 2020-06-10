015502ea51b219cfcdd4bd2f503c53ddffe712ea
Davans01 <Daniel@maikelvanstraten.nl>
2020-06-10 12:41:40 +0200

    updated swiftmailer

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,9 +14,27 @@ class DefaultController extends AbstractController
     /**
      * @Route("/default", name="default")
      */
-    public function index()
+    // Request $request werkt niet hij zegt dat die de class niet kan vinden terwijl ik hem wel heb toegevoegt
+    public function index(\Swift_Mailer $mailer)
     {
         $form = $this->createForm(ContactType::class);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $contactformdata = $form->GetData();
+
+            $message = (new \Swift_Message($contactFormData['subject']))
+            ->setFrom($contactFormData['from'])
+            ->setTo($this->getParameter('default_email'))
+            ->setBody(
+                   $contactFormData['comment'],
+                   'text/plain'
+                );
+
+           $mailer->send($comment);
+
+           return $this->redirectToRoute('contact');
+        }
 
         return $this->render('default/index.html.twig', [
            'our_form' => $form->createView()
